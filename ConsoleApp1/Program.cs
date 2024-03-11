90a2f00a674c8987fd5afa35022bc66564cdadce
beyoundthegraveee <jackertfun228@mail.ru>
2024-03-11 11:31:15 +0100

    newbranchcommit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
 
-Console.WriteLine("MOD1");
+class Program
+{
+
+    public static void Main(String[] args)
+    {
+        int[] tab = { 1, 5, 3, 2, 7 };
+        Console.WriteLine(count(tab));
+    }
+    public static int count(int[] tab)
+    {
+        int avg;
+        int sum = 0;
+        foreach (int tmp in tab)
+        {
+            sum += tmp;
+        }
+        
+        avg = sum / tab.Length;
+
+        return avg;
+    }
+    
+}
