5929de50f3f4cbc843c82c07308c900c821e54a1
beyoundthegraveee <jackertfun228@mail.ru>
2024-03-11 11:41:15 +0100

    feature-max commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,9 +7,10 @@ class Program
     public static void Main(String[] args)
     {
         int[] tab = { 1, 5, 3, 2, 7 };
-        Console.WriteLine(count(tab));
+        Console.WriteLine(CountAVG(tab));
+        Console.WriteLine(CountMAX(tab));
     }
-    public static int count(int[] tab)
+    public static int CountAVG(int[] tab)
     {
         int avg;
         int sum = 0;
@@ -21,6 +22,18 @@ class Program
         avg = sum / tab.Length;
 
         return avg;
+    }
+    
+    public static int CountMAX(int[] tab)
+    {
+        int max = 0;
+        foreach (int tmp in tab)
+        {
+            if (max < tmp)
+                max = tmp;
+        }
+
+        return max;
     }
     
 }
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90a2f00a674c8987fd5afa35022bc66564cdadce")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
